2dfb9b6a72b6436c6008ad8467b1096e53eb801d
Matteo Turchetta <matteotu@inf.ethz.ch>
2022-05-11 15:42:03 +0200

    ADD:ydoy to date conversion and viceversa

--- cyclesgym/envs/utils.py
+++ cyclesgym/envs/utils.py
@@ -1,13 +1,14 @@
 import os as _os
 import weakref
 from pathlib import Path
+import numpy as np
 
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from uuid import uuid4
 from tempfile import TemporaryDirectory
 
 
-__all__ = ['MyTemporaryDirectory', 'create_sim_id']
+__all__ = ['MyTemporaryDirectory', 'create_sim_id', 'date2ydoy', 'ydoy2date']
 
 
 class MyTemporaryDirectory(TemporaryDirectory):
@@ -27,3 +28,44 @@ class MyTemporaryDirectory(TemporaryDirectory):
 
 def create_sim_id():
     return datetime.now().strftime('%Y_%m_%d_%H_%M_%S-') + str(uuid4())
+
+
+def date2ydoy(date):
+    def date2ydoy_single(date):
+        tmp = date.timetuple()
+        return tmp.tm_year, tmp.tm_yday
+
+    if hasattr(date, '__iter__'):
+        y = []
+        doy = []
+        for d in date:
+            y_tmp, doy_tmp = date2ydoy_single(d)
+            y.append(y_tmp)
+            doy.append(doy_tmp)
+        return y, doy
+
+    else:
+        return date2ydoy_single(date)
+
+
+def ydoy2date(y, doy):
+    def ydoy2date_single(y, doy):
+        # Timedelta does not work with np.int64
+        if isinstance(y, np.int64):
+            y = int(y)
+        if isinstance(doy, np.int64):
+            doy = int(doy)
+        return date(y, 1, 1) + timedelta(doy - 1)
+
+    if hasattr(y, '__iter__') and hasattr(doy, '__iter__'):
+        if len(y) == len(doy):
+            dates = []
+            for y_tmp, doy_tmp in zip(y, doy):
+                dates.append(ydoy2date_single(y_tmp, doy_tmp))
+            return dates
+        else:
+            raise ValueError(f'year and doy list should have the same length. '
+                             f'They have {len(y)} and {len(doy)} lengths '
+                             f'instead.')
+    else:
+        return ydoy2date_single(y, doy)
