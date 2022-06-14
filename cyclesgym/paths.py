47364d137df3c400e351da934109fc5cba07be23
Luca Corinzia <luca.corinzia@inf.ethz.ch>
2022-06-14 09:58:24 +0200

    Update paths.py

--- cyclesgym/paths.py
+++ cyclesgym/paths.py
@@ -4,8 +4,7 @@ import os
 __all__ = ['PROJECT_PATH', 'CYCLES_PATH', 'AGENTS_PATH', 'FIGURES_PATH', 'DATA_PATH']
 
 PROJECT_PATH = pathlib.Path(__file__).parents[1]
-#CYCLES_PATH = PROJECT_PATH.joinpath('cycles')
-CYCLES_PATH = pathlib.Path(os.environ['TMPDIR']).joinpath('cycles')
+CYCLES_PATH = PROJECT_PATH.joinpath('cycles')
 AGENTS_PATH = PROJECT_PATH.joinpath('agents')
 FIGURES_PATH = PROJECT_PATH.joinpath('figures')
 DATA_PATH = PROJECT_PATH.joinpath('data')
