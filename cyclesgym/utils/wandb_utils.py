69a0d4c85f621882c83a9aabc8b5fdde67ea6bb0
Luca <luca.corinzia@gmail.com>
2022-06-16 19:33:55 +0200

    updated wandb entity for release

--- cyclesgym/utils/wandb_utils.py
+++ cyclesgym/utils/wandb_utils.py
@@ -1,5 +1,4 @@
-WANDB_ENTITY = 'koralabs'
-#TODO: change to None for the published version
+WANDB_ENTITY = None
 
 CROP_PLANNING_EXPERIMENT = 'experiments_crop_planning'
 FERTILIZATION_EXPERIMENT = 'agro-rl'
